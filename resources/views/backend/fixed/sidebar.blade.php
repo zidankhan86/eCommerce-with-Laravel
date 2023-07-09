0eb1a3474cb064eb9f0c1d4add02adb474f53c7f
zidankhan86 <zidankhan718@gmail.com>
2023-07-09 17:37:46 +0600

    dahboard link

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{route('dashboard')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
