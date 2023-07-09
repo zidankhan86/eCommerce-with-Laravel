5f3e3a65b193f99ee511bdd008e84d4e2997804a
zidankhan86 <zidankhan718@gmail.com>
2023-07-09 11:38:29 +0600

    templeating

---
+++ resources/views/backend/master.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="{{url('css/styles.css')}}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+
+
+
+        @include('backend.fixed.header')
+
+
+        <div id="layoutSidenav">
+
+
+
+
+            @include('backend.fixed.sidebar')
+
+
+            <div id="layoutSidenav_content">
+                <main>
+
+
+        @yield('content')
+
+
+
+                </main>
+
+
+
+
+     @include('backend.fixed.footer')
+
+
+
+
+
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{url('js/scripts.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="{{url('assets/demo/chart-area-demo.js')}}"></script>
+        <script src="{{url('assets/demo/chart-bar-demo.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="{{url('js/datatables-simple-demo.js')}}"></script>
+    </body>
+</html>
