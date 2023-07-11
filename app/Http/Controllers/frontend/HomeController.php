f362752d65952f83efbcb4eb927d51805d8500c3
zidankhan86 <zidankhan718@gmail.com>
2023-07-11 18:23:02 +0600

    home

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        return view('frontend.master');
+        return view('frontend.pages.home');
     }
 }
