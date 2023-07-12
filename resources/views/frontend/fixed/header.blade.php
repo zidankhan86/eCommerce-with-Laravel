650afab2d7d8831f5f9feeab75165ac5fbfc58bd
zidankhan86 <zidankhan718@gmail.com>
2023-07-12 13:04:30 +0600

    route

--- resources/views/frontend/fixed/header.blade.php
+++ resources/views/frontend/fixed/header.blade.php
@@ -46,7 +46,7 @@
             <div class="col-lg-6">
                 <nav class="header__menu">
                     <ul>
-                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li class="active"><a href="{{route('home')}}">Home</a></li>
                         <li><a href="{{route('product')}}">Shop</a></li>
                         <li><a href="#">Pages</a>
                             <ul class="header__menu__dropdown">
