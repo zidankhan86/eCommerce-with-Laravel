86900308ccec990b5bd6e983320a00db25d7e0b1
zidankhan86 <zidankhan718@gmail.com>
2023-07-12 12:49:40 +0600

    details

--- resources/views/frontend/pages/product/details.blade.php
+++ resources/views/frontend/pages/product/details.blade.php
@@ -8,10 +8,6 @@
 
 
   <body>
-     
-
-
-
 
 
       <!-- Hero Section Begin -->
@@ -94,7 +90,11 @@
                       <div class="product__details__pic">
                           <div class="product__details__pic__item">
                               <img class="product__details__pic__item--large"
-                                  src="img/product/details/product-details-1.jpg" alt="">
+
+
+                                  src="{{ asset('/public/uploads/' . $details->image) }}" alt="">
+
+
                           </div>
                           <div class="product__details__pic__slider owl-carousel">
                               <img data-imgbigurl="img/product/details/product-details-2.jpg"
@@ -110,7 +110,7 @@
                   </div>
                   <div class="col-lg-6 col-md-6">
                       <div class="product__details__text">
-                          <h3>Vetgetable’s Package</h3>
+                          <h3>{{$details->name}}</h3>
                           <div class="product__details__rating">
                               <i class="fa fa-star"></i>
                               <i class="fa fa-star"></i>
@@ -119,10 +119,8 @@
                               <i class="fa fa-star-half-o"></i>
                               <span>(18 reviews)</span>
                           </div>
-                          <div class="product__details__price">$50.00</div>
-                          <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam
-                              vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Vestibulum ac diam sit amet
-                              quam vehicula elementum sed sit amet dui. Proin eget tortor risus.</p>
+                          <div class="product__details__price">{{$details->price}}.00 Tk</div>
+                          <p>{{$details->description}}</p>
                           <div class="product__details__quantity">
                               <div class="quantity">
                                   <div class="pro-qty">
@@ -134,8 +132,8 @@
                           <a href="#" class="heart-icon"><span class="icon_heart_alt"></span></a>
                           <ul>
                               <li><b>Availability</b> <span>In Stock</span></li>
-                              <li><b>Shipping</b> <span>01 day shipping. <samp>Free pickup today</samp></span></li>
-                              <li><b>Weight</b> <span>0.5 kg</span></li>
+                              <li><b>Shipping</b> <span>{{$details->time}} Days inside Dhaka <samp>5 Days Outside</samp></span></li>
+                              <li><b>Weight</b> <span>{{$details->weight}} Kg</span></li>
                               <li><b>Share on</b>
                                   <div class="share">
                                       <a href="#"><i class="fa fa-facebook"></i></a>
