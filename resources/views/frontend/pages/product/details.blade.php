6f23de99f6bf03e8e90e2d9903d22ded4e74ecf2
zidankhan86 <zidankhan718@gmail.com>
2023-07-13 11:28:57 +0600

    details product

--- resources/views/frontend/pages/product/details.blade.php
+++ resources/views/frontend/pages/product/details.blade.php
@@ -165,45 +165,21 @@
                               <div class="tab-pane active" id="tabs-1" role="tabpanel">
                                   <div class="product__details__tab__desc">
                                       <h6>Products Infomation</h6>
-                                      <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
-                                          Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Vivamus
-                                          suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam
-                                          vehicula elementum sed sit amet dui. Donec rutrum congue leo eget malesuada.
-                                          Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat,
-                                          accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a
-                                          pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula
-                                          elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus
-                                          et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam
-                                          vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
-                                          <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem
-                                          ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet
-                                          elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum
-                                          porta. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus
-                                          nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
-                                          Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed
-                                          porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum
-                                          sed sit amet dui. Proin eget tortor risus.</p>
+
+
+
+                                      <p> <p>{{$details->product_information}}</p></p>
+
+
+
+                                          
                                   </div>
                               </div>
                               <div class="tab-pane" id="tabs-2" role="tabpanel">
                                   <div class="product__details__tab__desc">
                                       <h6>Products Infomation</h6>
-                                      <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
-                                          Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus.
-                                          Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam
-                                          sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo
-                                          eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.
-                                          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent
-                                          sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac
-                                          diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante
-                                          ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                                          Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
-                                          Proin eget tortor risus.</p>
-                                      <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem
-                                          ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet
-                                          elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum
-                                          porta. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus
-                                          nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+                                      <p>{{$details->product_information}}</p>
+
                                   </div>
                               </div>
                               <div class="tab-pane" id="tabs-3" role="tabpanel">
