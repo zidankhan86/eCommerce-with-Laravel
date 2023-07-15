aad288ef6f53c8e2296585cc8688a8b735e80e1a
zidankhan86 <zidankhan718@gmail.com>
2023-07-15 22:08:18 +0600

    old value

--- resources/views/backend/pages/banner/bannerForm.blade.php
+++ resources/views/backend/pages/banner/bannerForm.blade.php
@@ -15,7 +15,7 @@
 
         <div class="mb-3 mx-sm-2">
         <label for="exampleInputName1" class="form-label">Banner Tittle</label>
-        <input type="text" class="form-control" id="exampleInputName1" name="tittle" placeholder="Dry Food..">
+        <input type="text" class="form-control" value="{{ old('tittle') }}" id="exampleInputName1" name="tittle" placeholder="Dry Food..">
         @error('tittle')
 
         <strong class="text-danger">{{$message}}</strong>
@@ -25,7 +25,7 @@
 
         <div class="mb-3 mx-sm-2">
         <label for="exampleInputName2" class="form-label">Short Description</label>
-        <input type="text" class="form-control" id="exampleInputName2" name="description" placeholder="100% Pure Juice..">
+        <input type="text" class="form-control" value="{{ old('description') }}" id="exampleInputName2" name="description" placeholder="100% Pure Juice..">
         @error('description')
 
         <strong class="text-danger">{{$message}}</strong>
@@ -35,7 +35,7 @@
 
             <div class="mb-3 mx-sm-2">
             <label for="exampleInputName2" class="form-label">Upload Image</label>
-            <input type="file" class="form-control" id="exampleInputName2" name="image">
+            <input type="file" class="form-control" value="{{ old('image') }}" id="exampleInputName2" name="image">
             @error('image')
 
             <strong class="text-danger">{{$message}}</strong>
