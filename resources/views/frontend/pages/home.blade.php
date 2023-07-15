76e26c9d156349347e7ee1d949cdbbee60dc74b1
zidankhan86 <zidankhan718@gmail.com>
2023-07-16 00:32:47 +0600

    latest product

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -181,7 +181,7 @@
 <!-- Banner End -->
 
 <!-- Latest Product Section Begin -->
-<section class="latest-product spad">
+{{-- <section class="latest-product spad">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-6">
@@ -191,7 +191,7 @@
                         <div class="latest-prdouct__slider__item">
                             <a href="#" class="latest-product__item">
                                 <div class="latest-product__item__pic">
-                                    <img src="{{url('frontend/img/latest-product/lp-1.jpg')}}" alt="">
+                                    <img src="{{url('/frontend/img/latest-product/lp-1.jpg')}}" alt="">
                                 </div>
                                 <div class="latest-product__item__text">
                                     <h6>Crab Pool Security</h6>
@@ -381,8 +381,69 @@
             </div>
         </div>
     </div>
-</section>
+</section> --}}
 <!-- Latest Product Section End -->
+
+
+<section class="featured spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2>Latest Product</h2>
+                </div>
+                <div class="featured__controls">
+                    <ul>
+                        <li class="active" data-filter="*">All</li>
+                        <li data-filter=".oranges">Oranges</li>
+                        <li data-filter=".fresh-meat">Fresh Meat</li>
+                        <li data-filter=".vegetables">Vegetables</li>
+                        <li data-filter=".fastfood">Fastfood</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row featured__filter">
+
+            @foreach ($products as $item)
+
+            <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
+                <div class="featured__item">
+                    <div class="featured__item__pic">
+                        <a href="{{route('product.details',$item->id)}}"> <img src="{{ asset('/public/uploads/' . $item->image) }}" alt="Product Image"></a>
+                        <ul class="featured__item__pic__hover">
+                            {{-- <li><a href="#"><i class="fa fa-heart"></i></a></li> --}}
+                            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
+                            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="featured__item__text">
+                        <h6><a href="#">{{$item->name}}</a></h6>
+                        <h5>{{$item->price}} Tk.</h5>
+                    </div><br>
+
+
+                    {{-- <a href="{{route('product.details',$item->id)}}" class="btn btn-info btn-lg" style="width: 150px;">Details</a>
+                    <a href="" class="btn btn-success btn-lg" style="width: 100px;">Order</a> --}}
+
+
+
+                </div>
+
+
+            </div>
+
+
+            @endforeach
+
+
+
+
+
+
+        </div>
+    </div>
+</section>
 
 <!-- Blog Section Begin -->
 <section class="from-blog spad">
