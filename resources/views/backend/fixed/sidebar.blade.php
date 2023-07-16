9450f97c402d8e9c774b366f07b9d94c244a58a4
zidankhan86 <zidankhan718@gmail.com>
2023-07-16 23:08:30 +0600

    edited

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -17,7 +17,7 @@
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="{{route('category.list')}}">Add Category</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                        
                     </nav>
                 </div>
 
