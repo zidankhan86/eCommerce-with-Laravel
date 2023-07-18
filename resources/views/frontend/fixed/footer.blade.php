27d948f04c520e8f6f198d3ddf93bb7cce36f253
zidankhan86 <zidankhan718@gmail.com>
2023-07-18 12:59:42 +0600

    edited

--- resources/views/frontend/fixed/footer.blade.php
+++ resources/views/frontend/fixed/footer.blade.php
@@ -55,9 +55,9 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="footer__copyright">
-                    <div class="footer__copyright__text"><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved  by DTerms
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
+                    <div class="footer__copyright__text"><p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved  by DTerms Limited
+</p></div>
                     <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
                 </div>
             </div>
