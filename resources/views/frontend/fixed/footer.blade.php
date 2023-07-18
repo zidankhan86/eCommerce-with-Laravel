2f72872b5ec14c39a6c6ff04097b6e63d7122eb4
zidankhan86 <zidankhan718@gmail.com>
2023-07-18 12:58:30 +0600

    links

--- resources/views/frontend/fixed/footer.blade.php
+++ resources/views/frontend/fixed/footer.blade.php
@@ -44,10 +44,10 @@
                         <button type="submit" class="site-btn">Subscribe</button>
                     </form>
                     <div class="footer__widget__social">
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-instagram"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                        <a href="https://www.facebook.com/nongorfoodbd"><i class="fa fa-facebook"></i></a>
+                        <a href="https://www.instagram.com/nongorfood/"><i class="fa fa-instagram"></i></a>
+                        <a href="https://twitter.com/nongorfood"><i class="fa fa-twitter"></i></a>
+                        <a href="https://www.youtube.com/channel/UC-0rsi0dXMF_k1zKjXZOAqw"><i class="fa fa-youtube"></i></a>
                     </div>
                 </div>
             </div>
@@ -56,7 +56,7 @@
             <div class="col-lg-12">
                 <div class="footer__copyright">
                     <div class="footer__copyright__text"><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved  by DTerms
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
                     <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
                 </div>
