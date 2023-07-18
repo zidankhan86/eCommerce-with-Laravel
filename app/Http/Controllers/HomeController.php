91e68c71314eabc1b52986353f8f9f5f84ba6809
zidankhan86 <zidankhan718@gmail.com>
2023-07-18 12:52:48 +0600

    count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function dashboard(){
-        return view('backend.pages.dashboard');
+
+        $totalOrder = Order::get()->count();
+        $totalProducts = Product::get()->count();
+        $totalCategories = Category::get()->count();
+
+
+        return view('backend.pages.dashboard',compact('totalOrder','totalProducts','totalCategories'));
     }
 }
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -14,7 +14,7 @@
             <div class="card bg-primary text-white mb-4">
                 <div class="card-body">Primary Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <a class="small text-white stretched-link" href="#">{{$totalOrder}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
@@ -23,7 +23,7 @@
             <div class="card bg-warning text-white mb-4">
                 <div class="card-body">Warning Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <a class="small text-white stretched-link" href="#">{{$totalProducts}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
@@ -32,7 +32,7 @@
             <div class="card bg-success text-white mb-4">
                 <div class="card-body">Success Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <a class="small text-white stretched-link" href="#">{{$totalCategories}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
