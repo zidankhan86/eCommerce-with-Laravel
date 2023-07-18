4d13dfc294e31ab1d310529a425087b1f61fa5b5
zidankhan86 <zidankhan718@gmail.com>
2023-07-18 12:13:10 +0600

    links

--- resources/views/frontend/fixed/header.blade.php
+++ resources/views/frontend/fixed/header.blade.php
@@ -14,10 +14,11 @@
                 <div class="col-lg-6 col-md-6">
                     <div class="header__top__right">
                         <div class="header__top__right__social">
-                            <a href="#"><i class="fa fa-facebook"></i></a>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                            <a href="#"><i class="fa fa-linkedin"></i></a>
-                            <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                            <a href="https://www.facebook.com/nongorfoodbd"><i class="fa fa-facebook"></i></a>
+                            <a href="https://twitter.com/nongorfood"><i class="fa fa-twitter"></i></a>
+                            <a href="https://www.facebook.com/nongorfoodbd"><i class="fa fa-linkedin"></i></a>
+                            <a href="https://www.linkedin.com/company/96106466/admin/feed/posts/"><i class="fa fa-instagram"></i></a>
+                            <a href="https://www.youtube.com/channel/UC-0rsi0dXMF_k1zKjXZOAqw"><i class="fa fa-youtube"></i></a>
                         </div>
                         <div class="header__top__right__language">
 
