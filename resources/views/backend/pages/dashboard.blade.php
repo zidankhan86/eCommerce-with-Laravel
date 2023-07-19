21994235db708fb1e06d8dcbd327ee88059abf92
zidankhan86 <zidankhan718@gmail.com>
2023-07-19 17:22:35 +0600

    Edited

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -12,7 +12,7 @@
     <div class="row">
         <div class="col-xl-3 col-md-6">
             <div class="card bg-primary text-white mb-4">
-                <div class="card-body">Primary Card</div>
+                <div class="card-body">TOTAL NUMBER OF ORDERS</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">{{$totalOrder}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -21,7 +21,7 @@
         </div>
         <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Warning Card</div>
+                <div class="card-body">TOTAL NUMBER OF PRODUCTS</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">{{$totalProducts}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -30,7 +30,7 @@
         </div>
         <div class="col-xl-3 col-md-6">
             <div class="card bg-success text-white mb-4">
-                <div class="card-body">Success Card</div>
+                <div class="card-body">TOTAL NUMBER OF CATEGORIES</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">{{$totalCategories}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
