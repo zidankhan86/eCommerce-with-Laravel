ed1ccb118bfca751079a9c308ea302e87fa70719
zidankhan86 <zidankhan718@gmail.com>
2023-07-19 17:43:03 +0600

    link added

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -32,7 +32,7 @@
             <div class="card bg-success text-white mb-4">
                 <div class="card-body">TOTAL NUMBER OF CATEGORIES</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">{{$totalCategories}}</a>
+                    <a class="small text-white stretched-link" href="{{route('category.list')}}">{{$totalCategories}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
