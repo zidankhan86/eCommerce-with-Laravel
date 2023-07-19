54046e5daceb9a5a62f7b376bb1e4f7514b4deed
zidankhan86 <zidankhan718@gmail.com>
2023-07-19 17:39:50 +0600

    link added

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -14,7 +14,7 @@
             <div class="card bg-primary text-white mb-4">
                 <div class="card-body">TOTAL NUMBER OF ORDERS</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">{{$totalOrder}}</a>
+                    <a class="small text-white stretched-link" href="{{route('order.list')}}">{{$totalOrder}}</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
