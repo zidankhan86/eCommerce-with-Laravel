964dcb0288066304c7048eabf528896f4eebf134
zidankhan86 <zidankhan718@gmail.com>
2023-07-20 17:55:25 +0600

    value

--- resources/views/backend/pages/category/edit.blade.php
+++ resources/views/backend/pages/category/edit.blade.php
@@ -16,7 +16,7 @@
 
         <div class="mb-3 mx-sm-2">
         <label for="exampleInputName1" class="form-label">Category Name</label>
-        <input type="text" value="{{ old('name') }}" class="form-control" id="exampleInputName1" name="name" placeholder="Category Name..">
+        <input type="text" value="{{ $edit->name }}" class="form-control" id="exampleInputName1" name="name" placeholder="Category Name..">
         @error('name')
 
         <strong class="text-danger">{{$message}}</strong>
@@ -26,7 +26,7 @@
 
         <div class="mb-3 mx-sm-2">
         <label for="exampleInputName2" class="form-label">Category Type</label>
-        <input type="text" value="{{ old('type') }}" class="form-control" id="exampleInputName2" name="type" placeholder="Category Type..">
+        <input type="text" value="{{$edit->type}}" class="form-control" id="exampleInputName2" name="type" placeholder="Category Type..">
         @error('type')
 
         <strong class="text-danger">{{$message}}</strong>
