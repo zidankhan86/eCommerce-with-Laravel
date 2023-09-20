dc60d6aa2e08b02eab0db2e89886e2eb6dd9c36d
zidankhan86 <zidankhan718@gmail.com>
2023-09-20 23:39:19 +0600

    login form

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -53,5 +53,7 @@
         <script src="{{url('assets/demo/chart-bar-demo.js')}}"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="{{url('js/datatables-simple-demo.js')}}"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     </body>
 </html>
