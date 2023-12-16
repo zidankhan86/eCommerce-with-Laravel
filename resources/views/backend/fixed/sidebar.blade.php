5aabcfc009ce91ffa4a9ad2ff179722c55c75402
zidankhan86 <zidankhan718@gmail.com>
2023-12-17 01:29:05 +0600

    fix: icon issue

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -2,21 +2,22 @@
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <div class="sb-sidenav-menu-heading">Core</div>
+                
                 <a class="nav-link" href="{{route('dashboard')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-house"></i></div>
                     Dashboard
                 </a>
                 <div class="sb-sidenav-menu-heading">Home</div>
 
                 <a class="nav-link collapsed" href="" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-list"></i>
+                </div>
                     Categories
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{route('category.list')}}">Category</a>
+                        <a class="nav-link" href="{{route('category.list')}}"> Category</a>
                     </nav>
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="{{route('sub.category.form')}}">Sub Category</a>
@@ -25,7 +26,7 @@
 
 
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"  aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-cart-plus"></i></div>
                     Product
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
@@ -36,32 +37,29 @@
 
                           <a class="nav-link" href="{{route('product.list')}}">Product</a>
 
-                        {{-- <a class="nav-link collapsed" href="{{route('new.arrival.product.list')}}">
-                            New Arrival
-
-                        </a> --}}
+                      
 
                     </nav>
 
                 </div>
 
                 <a class="nav-link" href="{{route('order.list')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-cart-plus"></i></div>
                     Orders
                 </a>
                 <a class="nav-link" href="{{route('logo.list')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-ribbon"></i></div>
                    Company Logo
                 </a>
 
                 <a class="nav-link" href="{{route('hero.list')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-feather"></i></div>
                     Hero Banner
                 </a>
 
 
                 <a class="nav-link collapsed" href="" data-bs-toggle="collapse" data-bs-target="#collapseBanners" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-feather"></i></div>
                     Banners
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
@@ -80,17 +78,17 @@
                 </div>
 
                 <a class="nav-link" href="{{route('blog.list')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-blog"></i></div>
                     Blogs
                 </a>
 
                 <a class="nav-link" href="{{route('contact.list')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-comment"></i></div>
                     User Feedback
                 </a>
 
                 <a class="nav-link" href="{{route('report')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-print"></i></div>
                     Print Report
                 </a>
             </div>
