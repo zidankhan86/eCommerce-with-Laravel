adc50e24fb8777c6feb8fd96106dd03f3d272766
zidankhan86 <zidankhan718@gmail.com>
2024-01-05 01:59:34 +0600

    fix: trending product in home page

--- resources/views/frontend/pages/product/latest-product-squad.blade.php
+++ resources/views/frontend/pages/product/latest-product-squad.blade.php
@@ -96,33 +96,30 @@
                                 
                             </div>
                             <div class="latest-prdouct__slider__item">
-                                <a href="#" class="latest-product__item">
-                                    <div class="latest-product__item__pic">
-                                        <img src="img/latest-product/lp-1.jpg" alt="">
-                                    </div>
-                                    <div class="latest-product__item__text">
-                                        <h6>Crab Pool Security</h6>
-                                        <span>$30.00</span>
-                                    </div>
-                                </a>
-                                <a href="#" class="latest-product__item">
-                                    <div class="latest-product__item__pic">
-                                        <img src="img/latest-product/lp-2.jpg" alt="">
-                                    </div>
-                                    <div class="latest-product__item__text">
-                                        <h6>Crab Pool Security</h6>
-                                        <span>$30.00</span>
-                                    </div>
-                                </a>
-                                <a href="#" class="latest-product__item">
-                                    <div class="latest-product__item__pic">
-                                        <img src="img/latest-product/lp-3.jpg" alt="">
-                                    </div>
-                                    <div class="latest-product__item__text">
-                                        <h6>Crab Pool Security</h6>
-                                        <span>$30.00</span>
-                                    </div>
-                                </a>
+
+
+                               @php
+                $trendingProduct = \App\Models\Product::where('status', 2)->latest()->limit(3)->get();
+               
+                            @endphp
+                                
+                               
+                                @if($trendingProduct->isNotEmpty())
+                                @foreach($trendingProduct as $product)
+                                    <a href="#" class="latest-product__item">
+                                        <div class="latest-product__item__pic">
+                                            <img src="{{ asset('/public/uploads/' . $product->image) }}" alt="{{ $product->name }}">
+                                        </div>
+                                        <div class="latest-product__item__text">
+                                            <h6>{{ $product->name }}</h6>
+                                            <span>${{ $product->price }}</span> {{-- Assuming there's a 'price' attribute in your Product model --}}
+                                        </div>
+                                    </a>
+                                @endforeach
+                            @else
+                                <p>No latest products available.</p>
+                            @endif
+                              
                             </div>
                         </div>
 
