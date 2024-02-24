107caeb37ddac201c179d3e5429fbd8f36aaa328
zidankhan86 <zidankhan718@gmail.com>
2024-02-25 01:58:29 +0600

    fix: social share post blade

---
+++ resources/views/frontend/pages/product/posts.blade.php
@@ -0,0 +1,37 @@
+@extends('frontend.master')
+@section('content')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Implement Social Share Button in Laravel</title>
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+        <style>
+            div#social-links {
+                margin: 0 auto;
+                max-width: 500px;
+            }
+            div#social-links ul li {
+                display: inline-block;
+            }          
+            div#social-links ul li a {
+                padding: 20px;
+                border: 1px solid #ccc;
+                margin: 1px;
+                font-size: 30px;
+                color: #222;
+                background-color: #ccc;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <h2 class="mb-5 text-center">Laravel Social Share Buttons Example</h2>
+            {!! $shareComponent !!}
+        </div>
+    </body>
+</html>
+@endsection
