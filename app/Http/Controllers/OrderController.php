7c76e02f7097477681448c0e51841725881c09f9
zidankhan86 <zidankhan718@gmail.com>
2024-05-21 20:56:44 +0600

    fix:order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -23,4 +23,42 @@ class OrderController extends Controller
         Order::find($id);
         return view('frontend.pages.orderTrack.orderTrack');
     }
+
+    public function report()
+    {
+        //dd('yes');
+        $oders = Order::all();
+        
+        return view('backend.pages.report.report',compact('oders'));
+    }
+
+    public function reportSearch(Request $request)
+    {
+
+    //dd('yes');
+        $validator = Validator::make($request->all(), [
+            'from_date'    => 'required|date',
+            'to_date'      => 'required|date|after:from_date',
+        ]);
+
+        if($validator->fails())
+        {
+
+           Alert:: toast()->error('From date and to date required and to should greater then from date.');
+            return redirect()->back();
+        }
+
+
+
+       $from=$request->from_date;
+       $to=$request->to_date;
+
+
+//       $status=$request->status;
+
+
+        $orders=Order::whereBetween('created_at', [$from, $to])->get();
+        return view('backend.pages.report.report',compact('orders'));
+
+    }
 }
